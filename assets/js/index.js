1df0fe884f600b0029ddc56dafaa5734e601c924
Oliver <oliver@pixel.id.au>
2020-03-09 19:50:59 +1000

    Auto deploy

--- assets/js/index.js
+++ assets/js/index.js
@@ -122,6 +122,14 @@ $("select").change(function () {
     $(".stdtext").html('<p class="text-muted" id="msginfo">This message doesn\'t require any extra info.</p>');
   }
 
+  // Does the message have Name?
+  const message = dropdownMap.get(code);
+  if (message.modname.length >= 1) {
+    $(".stdtext").html('<input class="form-control" type="text" placeholder="' + message.modname[0] + '" id="mod">');
+  } else {
+    $(".stdtext").html('<p class="text-muted" id="msginfo">This message doesn\'t require a name.</p>');
+  }
+
   // Enable copy button
   $("#copybutton").removeAttr("disabled");
 });
